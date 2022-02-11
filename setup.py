3d0fde9f651bd12fc86daa1b1cfe33b28aa35ada
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-02-11 11:52:23 +0100

    dependencies added.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="playground",
     version="0.0.1",
     author="Antoine Stevan",
-    # author_email="author@example.com",
+    author_email="Antoine.STEVAN@student.isae-supaero.fr",
     description="The modified environment of the IMAGINE research paper.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,4 +23,10 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[
+        "gym>=0.21.0",
+        "matplotlib>=3.5.1",
+        "numpy>=1.22.2",
+        "pygame>=2.1.2",
+    ],
 )
