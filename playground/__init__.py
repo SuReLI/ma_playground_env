3bebaeb0d652592e0556f07125e621a29d6d3dda
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-02-11 13:40:30 +0100

    moved src/playground to playground.

--- playground/__init__.py
+++ playground/__init__.py
@@ -1,18 +1,17 @@
-import itertools
 from gym.envs.registration import register
-import numpy as np
 
+print("REGISTRATION")
 for v in ['1']:
     register(id='PlaygroundNavigation-v' + v,
-             entry_point='playgroundnavv' + v + ':PlayGroundNavigationV' + v,
+             entry_point='playground.playgroundnavv' + v + ':PlayGroundNavigationV' + v,
              max_episode_steps=50)
 
     register(id='PlaygroundNavigationHuman-v' + v,
-             entry_point='playgroundnavv' + v + ':PlayGroundNavigationV' + v,
+             entry_point='playground.playgroundnavv' + v + ':PlayGroundNavigationV' + v,
              max_episode_steps=50,
              kwargs=dict(human=True, render_mode=True))
 
     register(id='PlaygroundNavigationRender-v' + v,
-             entry_point='playgroundnavv' + v + ':PlayGroundNavigationV' + v,
+             entry_point='playground.playgroundnavv' + v + ':PlayGroundNavigationV' + v,
              max_episode_steps=50,
              kwargs=dict(human=False, render_mode=True))
